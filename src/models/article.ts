e51f7078164a863ac710c1bbd11bad4f0fb87a7e
Crayee <till.gohl@gmx.de>
2024-03-28 12:11:39 +0000

    add models for user and article

---
+++ src/models/article.ts
@@ -0,0 +1,30 @@
+import mongoose, { Model } from "mongoose";
+
+const { SchemaTypes } = mongoose;
+const { String, Date: MDate, ObjectId } = SchemaTypes;
+
+
+export type Article = {
+    _id: string
+    title: string
+    content: string
+    authorId: string
+
+    createdAt: Date
+    editedAt: Date | null
+}
+
+const SCHEMA = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    authorId: { type: ObjectId, required: true },
+
+    createdAt: { type: MDate, required: true },
+    editedAt: { type: MDate },
+});
+
+type ArticleEntity = Omit<Article, "_id"> & { _id: any };
+export type ArticleDocument = mongoose.Document & ArticleEntity;
+
+export const getArticleDocument = (): Model<ArticleDocument> =>
+    mongoose.model<ArticleDocument>("Article", SCHEMA, "article");
---
+++ src/models/user.ts
@@ -0,0 +1,36 @@
+import mongoose, { Model } from "mongoose";
+
+const { SchemaTypes } = mongoose;
+const { String, Date: MDate } = SchemaTypes;
+
+
+export type User = {
+    _id: string
+    username: string
+    email: string
+    firstName: string | null
+    lastName: string | null
+
+    passwordHash: string
+    salt: string
+
+    createdAt: Date
+}
+
+const SCHEMA = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type: String },
+
+    passwordHash: { type: String, required: true },
+    salt: { type: String, required: true },
+
+    createdAt: { type: MDate, required: true }
+});
+
+type UserEntity = Omit<User, "_id"> & { _id: any };
+export type UserDocument = mongoose.Document & UserEntity;
+
+export const getUserModel = (): Model<UserDocument> =>
+    mongoose.model<UserDocument>("User", SCHEMA, "user");
