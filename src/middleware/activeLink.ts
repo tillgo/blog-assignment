b85b7f9e997c3847c3fa8030582b5dc0e789d978
tillgo <till.gohl@gmx.de>
2024-04-10 21:13:28 +0100

    add quilljs

--- src/middleware/activeLink.ts
+++ src/middleware/activeLink.ts
@@ -9,7 +9,6 @@ import { NextFunction, Request, Response } from 'express'
  */
 export const activeLink = async (req: Request, res: Response, next: NextFunction) => {
     res.locals.activePath = req.path
-    console.log(req.path)
 
     next()
 }
