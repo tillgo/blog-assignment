27b0e8a4cd27ad4c3709860cb02eb59026c55c26
tillgo <till.gohl@gmx.de>
2024-04-11 19:07:42 +0100

    clear alert timeout on new alert and dismiss

--- public/scripts/alerts.js
+++ public/scripts/alerts.js
@@ -1,9 +1,20 @@
+let timeout = null
+
 const handleCloseAlert = () => {
+    if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+    }
     const alert = document.getElementById('alert-snack')
     alert.classList.add('hidden')
 }
 
 const handleError = async (response) => {
+    if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+    }
+
     const data = await response.json()
     const label = document.getElementById('alert-message')
     label.innerText = data.message ?? 'An unexpected error occurred.'
@@ -12,7 +23,7 @@ const handleError = async (response) => {
     alert.classList.remove('hidden')
 
     // timeout to hide the snack after 6 seconds automatically
-    setTimeout(() => {
+    timeout = setTimeout(() => {
         alert.classList.add('hidden')
     }, 6000)
 }
